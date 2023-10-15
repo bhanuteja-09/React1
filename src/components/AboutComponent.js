cefd26e9eda745b3bfd7f0067f55b9700430f96f
Bhanu Tejaswini Kaluva <bhanutejaswinikaluva@gmail.com>
2023-10-15 23:20:44 +0530

    modify3

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -130,7 +130,7 @@ import { Link } from 'react-router-dom';
             </div>
             <div className="row row-content">
                 <div className="col-12">
-                    {/* <h2>Corporate Leadership</h2> */}
+                    <h2>Corporate Leadership</h2>
                 </div>
                 <div className="col-12">
                     <Media list>
